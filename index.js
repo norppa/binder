bcfa8b84cb22b1cb293516beef3a126f7b4cdc54
Jari Haavisto <jari.haavisto@bisnode.com>
2019-08-16 12:27:19 +0300

    add mysql & dotenv

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const pool = require('./db.js')
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/:site', (req, res) => {
+    pool.query('SELECT * FROM bdr_test', (error, result) => {
+        console.log(error, result)
+    })
+    res.send(req.params)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
