22ddf75e081cbe29940ae3ab5bf54d84206068af
Jari Haavisto <jari.haavisto@bisnode.com>
2019-08-16 12:42:18 +0300

    set different webpack conf for dev & prod

--- front/webpack.common.js
+++ front/webpack.common.js
@@ -3,10 +3,6 @@ var HtmlWebpackPlugin =  require('html-webpack-plugin');
 
 module.exports = {
     entry : './app/index.js',
-    output : {
-        path : path.resolve(__dirname , 'dist'),
-        filename: 'index_bundle.js'
-    },
     module : {
         rules : [
             {test : /\.(js)$/, use:'babel-loader'},
---
+++ front/webpack.dev.js
@@ -0,0 +1,15 @@
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+var path = require('path')
+
+module.exports = merge(common, {
+  mode: 'development',
+  output : {
+      path : path.resolve(__dirname , 'dist'),
+      filename: 'binder.bundle.js',
+      publicPath: '/'
+  },
+  devServer: {
+      historyApiFallback: true,
+  },
+})
